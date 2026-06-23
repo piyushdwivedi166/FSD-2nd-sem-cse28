eb8d10560bcaadea1740692fe15b7b7db701e116
piyushdwivedi166 <piyushdwivedi049166@gmail.com>
2026-06-23 10:06:03 +0530

    done

---
+++ REACTcode/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello React 🚀</h1>
+      <p>My first React app</p>
+      <h1>csk ke chode shivansh bsdk </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ script1.js
@@ -0,0 +1,11 @@
+
+console.log("hello world");
+var a=10;
+var b=20;
+var c=a+b;
+console.log(c);
+console.log(let introduction);
+let a=10;
+a=90;
+console.log(a);
+const x=100;
